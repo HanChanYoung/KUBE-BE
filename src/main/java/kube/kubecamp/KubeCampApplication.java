648f77d11f991977d858c82cfbec15db39cf044b
Chanyoung Han <cksdudals@gmail.com>
2023-02-09 11:41:51 +0900

    [CAMP-45] FIX DB 부하분산 종속성 수정

--- src/main/java/kube/kubecamp/KubeCampApplication.java
+++ src/main/java/kube/kubecamp/KubeCampApplication.java
@@ -4,12 +4,13 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.salt.StringFixedSaltGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //종속성 순환 에러 제거
 @EnableEncryptableProperties
 public class KubeCampApplication {
 
