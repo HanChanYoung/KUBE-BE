fad1155f67059a71e1d059569a7b10d0e76d8de1
Chanyoung Han <cksdudals@gmail.com>
2023-02-16 17:48:05 +0900

    [CAMP-45] FIX RedisboardGetAllRepo26

--- src/main/java/kube/kubecamp/data/dto/BoardDtoGet.java
+++ src/main/java/kube/kubecamp/data/dto/BoardDtoGet.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 @RedisHash(value="boardGet",timeToLive = 60)
 public class BoardDtoGet implements Serializable {
 
-    //private static final long serialVersionUID = -214490344996507077L;
+    private static final long serialVersionUID = -214490344996507077L;
 
     @Id
     @NotNull
